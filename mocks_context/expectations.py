798a724d940d7d7b30d8d2b1eaa3fac906abdc0b
Bernardo Fontes <bernardoxhc@gmail.com>
2019-07-18 16:39:37 -0300

    Add multiple calls expectation

--- mocks_context/expectations.py
+++ mocks_context/expectations.py
@@ -1,4 +1,5 @@
 from abc import ABCMeta, abstractmethod
+from unittest.mock import call
 
 
 class ExpectationInterface(metaclass=ABCMeta):
@@ -29,3 +30,40 @@ class SingleCallExpectation(ExpectationInterface):
         Will raise exception if unmatching args or kwargs or if called more than one time.
         """
         self.mocked_method.assert_called_once_with(*self.call_args, **self.call_kwargs)
+
+
+class MultipleCallsExpectation(ExpectationInterface):
+    """
+    Ensures that a mocked method has the specified calls respecting the expectations order by default.
+    Exposed attributes:
+    - any_order (default=False): if True ignores the calls order;
+                                 if False, satisfied will raise an AssertionError if calls order differs
+                                 from the expectations order
+    - match_count (default=False: if True, will check the mock call count againts the number of expected calls
+    - count (default=0): if not 0, will replace the number of expected calls in match count checking
+    """
+
+    def __init__(self, mocked_method, any_order=False, match_count=False):
+        self.mocked_method = mocked_method
+        self._calls = []
+        self.count = 0
+        self.any_order = any_order
+        self.match_count = match_count
+
+    def new_call_params(self, *args, **kwargs):
+        """
+        Will add the *args, **kwargs as an expected call in the expected calls list
+        """
+        self._calls.append(call(*args, **kwargs))
+
+    def satisfied(self):
+        if self.match_count:
+            expected_count = self.count or len(self._calls)
+            match_call_count = self.mocked_method.call_count == expected_count
+
+            if not match_call_count:
+                actual_calls = self.mocked_method.call_count
+                msg = f'Call count does not match for {self.mocked_method}: {actual_calls} calls from {expected_count} expected.'
+                assert match_call_count, msg
+
+        self.mocked_method.assert_has_calls(self._calls, any_order=self.any_order)
--- tests/test_expectations.py
+++ tests/test_expectations.py
@@ -2,7 +2,7 @@ import pytest
 from unittest import TestCase
 from unittest.mock import Mock
 
-from mocks_context.expectations import SingleCallExpectation
+from mocks_context.expectations import SingleCallExpectation, MultipleCallsExpectation
 
 
 class SingleCallExpectationTests(TestCase):
@@ -13,7 +13,7 @@ class SingleCallExpectationTests(TestCase):
 
     def test_successful_single_call_expectation(self):
         self.mocked_method(1, 2, b=3, c=4)
-        self.expectation.satisfied()  # shouldn't raise exception
+        assert self.expectation.satisfied() is None
 
     def test_error_on_single_call_expectation_due_to_missing_arg(self):
         self.mocked_method(1, b=3, c=4)
@@ -42,5 +42,125 @@ class SingleCallExpectationTests(TestCase):
 
     def test_error_on_single_call_expectation_due_to_extra_kwarg(self):
         self.mocked_method(1, 2, b=3, c=4, d=1000)
+        with pytest.raises(AssertionError):
+            self.expectation.satisfied()
+
+
+class MultipleCallsExpectationTests(TestCase):
+
+    def setUp(self):
+        self.mocked_method = Mock()
+        self.expectation = MultipleCallsExpectation(self.mocked_method)
+
+    def test_successful_multiple_calls_expectation(self):
+        self.expectation.new_call_params(1, 2, b=3, c=4)
+        self.expectation.new_call_params(4, 3, b=2, c=1)
+
+        self.mocked_method(1, 2, b=3, c=4)
+        self.mocked_method(4, 3, b=2, c=1)
+
+        assert self.expectation.satisfied() is None
+
+    def test_multiple_calls_expectation_ensure_order_by_default(self):
+        self.expectation.new_call_params(1, 2, b=3, c=4)
+        self.expectation.new_call_params(4, 3, b=2, c=1)
+
+        # calls in a different order from the expectations
+        self.mocked_method(4, 3, b=2, c=1)
+        self.mocked_method(1, 2, b=3, c=4)
+
+        with pytest.raises(AssertionError):
+            assert self.expectation.satisfied()
+
+        self.expectation.any_order = True
+        assert self.expectation.satisfied() is None
+
+    def test_successful_multiple_calls_ensuring_expected_calls_count(self):
+        self.expectation.match_count = True
+        self.expectation.new_call_params(1, 2, b=3, c=4)
+        self.expectation.new_call_params(4, 3, b=2, c=1)
+
+        self.mocked_method(1, 2, b=3, c=4)
+        self.mocked_method(4, 3, b=2, c=1)
+
+        assert self.expectation.satisfied() is None
+
+        # extra call should fail
+        self.mocked_method(4, 3, b=2, c=1)
+        with pytest.raises(AssertionError):
+            self.expectation.satisfied()
+
+    def test_successful_multiple_calls_ensuring_forced_call_count(self):
+        self.expectation.match_count = True
+        self.expectation.count = 10
+        self.expectation.new_call_params(1, 2, b=3, c=4)
+
+        for i in range(10):
+            self.mocked_method(1, 2, b=3, c=4)
+
+        assert self.expectation.satisfied() is None
+
+        # extra call should fail
+        self.mocked_method(1, 2, b=3, c=4)
+        with pytest.raises(AssertionError):
+            self.expectation.satisfied()
+
+    def test_error_on_multiple_call_expectation_due_to_missing_arg(self):
+        self.expectation.new_call_params(1, 2, b=3, c=4)
+        self.expectation.new_call_params(4, 3, b=2, c=1)
+
+        self.mocked_method(1, 2, b=3, c=4)
+        self.mocked_method(4, b=2, c=1)
+
+        with pytest.raises(AssertionError):
+            self.expectation.satisfied()
+
+    def test_error_on_multiple_call_expectation_due_to_extra_arg(self):
+        self.expectation.new_call_params(1, 2, b=3, c=4)
+        self.expectation.new_call_params(4, 3, b=2, c=1)
+
+        self.mocked_method(100, 1, 2, b=3, c=4)
+        self.mocked_method(4, 3, b=2, c=1)
+
+        with pytest.raises(AssertionError):
+            self.expectation.satisfied()
+
+    def test_error_on_multiple_call_expectation_due_to_incorrect_arg(self):
+        self.expectation.new_call_params(1, 2, b=3, c=4)
+        self.expectation.new_call_params(4, 3, b=2, c=1)
+
+        self.mocked_method(1, 2, b=3, c=4)
+        self.mocked_method(50, 3, b=2, c=1)
+
+        with pytest.raises(AssertionError):
+            self.expectation.satisfied()
+
+    def test_error_on_multiple_call_expectation_due_to_missing_kwarg(self):
+        self.expectation.new_call_params(1, 2, b=3, c=4)
+        self.expectation.new_call_params(4, 3, b=2, c=1)
+
+        self.mocked_method(1, 2, b=3)
+        self.mocked_method(4, 3, b=2, c=1)
+
+        with pytest.raises(AssertionError):
+            self.expectation.satisfied()
+
+    def test_error_on_multiple_call_expectation_due_to_incorrect_kwarg(self):
+        self.expectation.new_call_params(1, 2, b=3, c=4)
+        self.expectation.new_call_params(4, 3, b=2, c=1)
+
+        self.mocked_method(1, 2, b=3, c=1000)
+        self.mocked_method(4, 3, b=2, c=1)
+
+        with pytest.raises(AssertionError):
+            self.expectation.satisfied()
+
+    def test_error_on_multiple_call_expectation_due_to_extra_kwarg(self):
+        self.expectation.new_call_params(1, 2, b=3, c=4)
+        self.expectation.new_call_params(4, 3, b=2, c=1)
+
+        self.mocked_method(1, 2, b=3, c=4)
+        self.mocked_method(4, 3, b=2, c=1, f=10)
+
         with pytest.raises(AssertionError):
             self.expectation.satisfied()
